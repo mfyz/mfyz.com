4904e89f389e866616e4488d89537e1080ef69c2
Fatih Yildiz <fatih@mfyz.com>
2025-05-16 12:04:48 -0400

    bg patterns implemented for homepage grid

---
+++ src/utils/get-pattern-name.ts
@@ -0,0 +1,19 @@
+const PATTERNS = [
+  'cone',
+  'cube',
+  'triangle',
+  'wave',
+];
+
+export function getPatternName(slug) {
+  // Create a simple hash from the slug
+  const hash = slug.split('').reduce((acc, char) => {
+    return char.charCodeAt(0) + ((acc << 5) - acc);
+  }, 0);
+
+  // Use the absolute value of hash to ensure positive number
+  const index = Math.abs(hash) % PATTERNS.length;
+  
+  return PATTERNS[index];
+}
+
