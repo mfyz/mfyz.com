f8f10d47d1e96d54f10b6e3e14565e8b301894b5
Fatih Yildiz <fatih@mfyz.com>
2025-05-23 09:21:19 -0400

    2018 reviewed

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,6 +1,10 @@
 import PropTypes from 'prop-types';
 
-export default function Image({ src, alt, width, height, class: className }) {
+export default function Image({ src, alt, width, height, class: className, center }) {
+  if (center) {
+    className += " mx-auto";
+  }
+  
   return (
       <div className="img-wrapper">
         <img 
@@ -19,5 +23,6 @@ Image.propTypes = {
   alt: PropTypes.string.isRequired,
   width: PropTypes.number.isRequired,
   height: PropTypes.number.isRequired,
-  class: PropTypes.string
+  class: PropTypes.string,
+  center: PropTypes.bool
 }
