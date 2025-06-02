56a34cdc3f5de01912c122414f6c2fa9529e4ae8
Fatih Yildiz <fatih@mfyz.com>
2025-06-02 08:57:42 -0400

    og website url fixed and default image added

--- src/consts.js
+++ src/consts.js
@@ -1,10 +1,12 @@
 export const SITE = {
-  website: "https://mfyz.com",
+  website: "https://mfyz.com", // No trailing slash
   author: "Fatih Yildiz",
-  description: "A digital creator: Fatih Yildiz's notes on software engineering, ai, digital design, product and project management",
-  descriptionTr: "Dijital bir yaratıcısı: Fatih Yıldız'ın yazılım mühendisliği, AI, dijital tasarım, ürün ve proje yönetimi konusundaki notları",
+  description:
+    "A digital creator: Fatih Yildiz's notes on software engineering, ai, digital design, product and project management",
+  descriptionTr:
+    "Dijital bir yaratıcısı: Fatih Yıldız'ın yazılım mühendisliği, AI, dijital tasarım, ürün ve proje yönetimi konusundaki notları",
   title: "mfyz",
-  ogImage: "/og-image.png",
+  ogImage: "/images/og_default.png",
 };
 
 export const CATEGORIES = {
